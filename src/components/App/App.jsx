bec0e9321e2f20540063b087f925c3d0337a1d65
Jennifer Alexander <jenny_alexander@icloud.com>
2021-11-19 10:46:49 -0600

    initial commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,57 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { Container, Grid, Card, CardContent, Typography, Box } from '@mui/material';
 
 function App() {
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
+        <h1 className='App-title'>Make Yourself Heard</h1>
+        <h2 className='App-subtitle'>Feedback Form</h2>
       </header>
+      <body>
+        {/* <div id='main'>
+          <p>Tell me about your learning experience today.</p>
+          <button>Open Up</button>
+        </div> */}
+        
+        <Container>
+          <Grid container direction="column">
+            <Grid item xs={3}>
+              <Card 
+                sx={{
+
+                  height: 300,
+                  
+                }}
+              >
+                <CardContent>
+                  <Typography 
+                    variant="body1" 
+                    color="textSecondary" 
+                    component="p"
+                    align = "center"
+                  >
+                    Tell me about your learning experience.
+                  </Typography>
+
+                  <Typography 
+                    variant="body1" 
+                    color="textSecondary" 
+                    component="p"
+                    align = "center"
+                  >
+                    Tell me MORE about your learning experience.
+                  </Typography>
+
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </body>
     </div>
   );
 }
